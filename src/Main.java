ead686064ad0cfe107ece8ab1ae8be4d65b8e28c
OlgaRadzivilova <radzivilovaolga@gmail.com>
2022-04-14 20:56:20 +0300

    commit done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i++);
+        }
+        System.out.println();
+        for (int y = 10; y >= 1; y--) {
+            System.out.print(y);
+        }
+
+        // Задание 2
+
+        System.out.println();
+        int friday = 4;
+        for (; friday <= 31; friday = friday + 7) {
+            System.out.println("Today is Friday, the " + friday + "th. It is necessary to prepare a report!");
+        }
+
+        // Задание 3
+
+        int comet = 0;
+        int year = 2022;
+        for (; comet < year + 100 ; comet = comet + 79) {
+            if (comet > year - 200) {
+                System.out.println(comet);
+
+            }
+        }
     }
 }
